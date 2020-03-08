755429713525337c5e282ef7ae415317f6f3ded0
Prath23 <patyarahate@gmail.com>
2020-03-08 15:31:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# print (3 + 7)
+# print (4 * 5)
+# print (2 - 5)
+# print (type(22 / 7))
+# print (type(0.0))
+# print (6 ** 8)
+# print (7 // 3)
+# print (bin(1024))
+
+# user_name = 'pratham'
+# user_password = "learnPython"
+# long_str = '''
+# Hello there prath
+#  0   0 
+#    o
+#   ===
+
+# How are you doing? '''
+
+# print (long_str)
+
+# first_name = 'Prathamesh'
+# last_name = 'Rahate'
+# full_name = first_name + ' ' + last_name
+# print (full_name)
+
+
+#Formatted strings
+name = 'Johnny'
+age =  30
+#print ('Hi ' + name + ' you are ' + str(age) + ' years old.')
+# Below is the formatted string, feature of python 3
+print (f'Hi {name} you are {age} years old.')
