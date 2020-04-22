829b416afbee6a7fd711d50bfb0b0abab57b9251
Prathamesh Rahate <patyarahate@gmail.com>
2020-04-22 05:48:11 +0000

    For loop basics

--- conditionals_loops.py
+++ conditionals_loops.py
@@ -17,3 +17,23 @@ is_friend = False;
 can_mesage = "Messaging allowed" if is_friend else "Messaging Not Allowed"
 
 print(can_mesage)
+
+is_magician = True
+is_expert = False
+
+if (not is_magician):
+    print("You need magic powers")
+if (is_magician and is_expert):
+    print("You are master magician")
+if (is_magician and (not is_expert)):
+    print("Atleast you are getting there")
+
+# for Loop
+# here items is similar to i
+# and 'Hello world' cna be any iterable object
+for items in 'Hello World':
+    for i in [1,2]:
+        print(items, i)
+
+#for i in [1,2,3,4,5]:
+#    print(i)
--- main.py
+++ main.py
@@ -277,21 +277,48 @@ mytuple = (23,'Prath', True, 23)
 # Conditionals
 # if else
 
-is_old = False
-is_licensed = True
+#is_old = False
+#is_licensed = True
 
-if is_old:
-    print("You are old enough")
-elif is_licensed:
-    print("You are licensed to drive") 
-else:
-    print("You are not old enough")
+#if is_old:
+#    print("You are old enough")
+#elif is_licensed:
+#    print("You are licensed to drive") 
+#else:
+#    print("You are not old enough")
 
 # Ternery operator in python
 
 # condition_if_true if condition else condition_if_else
-is_friend = False;
+#is_friend = False;
 
-can_mesage = "Messaging allowed" if is_friend else "Messaging Not Allowed"
+#can_mesage = "Messaging allowed" if is_friend else "Messaging Not Allowed"
 
-print(can_mesage)
+#print(can_mesage)
+
+#if (4 > 5):
+#    print("4 is greater than 5")
+#if (4 < 5):
+#    print("4 is less than 5")
+#if (5 <= 5):
+#    print("True")
+
+#is_magician = True
+#is_expert = False
+
+#if (not is_magician):
+#    print("You need magic powers")
+#if (is_magician and is_expert):
+#    print("You are master magician")
+#if (is_magician and (not is_expert)):
+#    print("Atleast you are getting there")
+
+# for Loop
+# here items is similar to i
+# and 'Hello world' cna be any iterable object
+for items in 'Hello World':
+    for i in [1,2]:
+        print(items, i)
+
+#for i in [1,2,3,4,5]:
+#    print(i)
